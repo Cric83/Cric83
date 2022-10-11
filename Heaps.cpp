33017f0cdd35dbbb48a4e0241af253ae8c89d59a
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-10-11 10:29:19 +0530

    Update Heaps.cpp

--- Heaps.cpp
+++ Heaps.cpp
@@ -15,7 +15,7 @@ void Insert(int A[], int n)  // n is index of element we are going to insert
   A[i]=temp;
 
 }
-// this function is to display after heapify
+// this function is to display after heapify ..
 void Display(int B[])
 {
 	for(int i=1;i<=8;i++)
